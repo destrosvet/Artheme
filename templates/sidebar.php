6939bc6696f42fd8cec950ead7fa5edf4e42f31b
destrosvet <destrosvet@gmail.com>
2017-12-13 22:45:02 +0100

    fix load more fix feature post

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,6 +1,6 @@
 <aside id="sidebar" class="col-md-4 col-xs-12 sidebar-wrapper d-md-block d-sm-none text-center">
         <?php get_template_part('templates/widgets/dynamic'); ?>
-        <?php get_template_part('templates/widgets/sidebar-popular-posts') ?>
+        <?php //get_template_part('templates/widgets/sidebar-popular-posts') ?>
         <?php get_template_part('templates/widgets/newsletter') ?>
         <?php get_template_part('templates/widgets/dynamic2'); ?>
         <?php get_template_part('templates/widgets/sidebar-recent-comments') ?>
