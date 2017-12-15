5cd2c80068f95f24aa7dcb8b01332bf8679123f7
mato slebo <matejslebo@gmail.com>
2017-12-15 16:53:38 +0100

    Revert "Merge branch 'master' into development"
    This reverts commit 46cb887130330e8cbb704b2005ddf52d102d127f, reversing
    changes made to 3656cb215c4174228f77a73b9183d30f94143261.

--- templates/feature-slider.php
+++ templates/feature-slider.php
@@ -1,28 +1,29 @@
-
+<?php if ( $featured = artalk_get_featured_posts() ) : $i=0; ?>
       <div class="col-md-6 col-sm-12 col-xs-12 nopadding nomargin" id="featured">
-            <?php $query = new WP_Query( 'p=' . $sticky[0] ); ?>
-            <?php while ($query -> have_posts()) : $query->the_post(); ?>
-            <?php if ( $has_thumbnail = has_post_thumbnail() ) : ?>
+
+        <?php foreach ( $featured as $post ) : setup_postdata( $post ); $i++; ?>
+        <?php if ( $has_thumbnail = has_post_thumbnail() ) : ?>
 
 
-                <?php the_post_thumbnail('featured'); ?>
-                  <div class="columns medium-6">
-                    <a class="thumb-link" href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('featured'); ?>
+              <div class="columns medium-6">
+                <a class="thumb-link" href="<?php the_permalink(); ?>">
+                 
+                </a>
+              </div>
+            <?php endif; ?>
 
-                    </a>
-                  </div>
-                <?php endif; ?>
-
-                <div class="columns large-6 <?php echo $has_thumbnail ? 'medium-6':'medium-8 medium-centered'; ?>">
-                    <div class="inner">
-                        <h3 class="article-title main-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title() ?></a></h3>
-                        <div class="content excerpt">
-                          <?php echo artalk_get_the_excerpt($post->ID, 28); ?>
-                        </div>
-                        <?php get_template_part('templates/post_meta'); ?>
+            <div class="columns large-6 <?php echo $has_thumbnail ? 'medium-6':'medium-8 medium-centered'; ?>">
+                <div class="inner">
+                    <h3 class="article-title main-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title() ?></a></h3>
+                    <div class="content excerpt">
+                      <?php echo artalk_get_the_excerpt($post->ID, 28); ?>
                     </div>
+                    <?php get_template_part('templates/post_meta'); ?>
                 </div>
+            </div>
 
-            <?php endwhile; wp_reset_postdata(); ?>
+        <?php endforeach; wp_reset_postdata(); ?>
 
       </div>
+<?php endif; ?>
