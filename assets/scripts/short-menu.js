4bcb2e16df7e2866afd4d5617d0b573922245896
mato slebo <matejslebo@gmail.com>
2017-12-21 16:07:32 +0100

    fix responsiv menu

--- assets/scripts/short-menu.js
+++ assets/scripts/short-menu.js
@@ -2,10 +2,10 @@
  * Responsive short menu for version(480px - 768px)
  */
 $(document).ready(function() {
-    if($(window).width() > 769 && $(window).width() < 990 ){
+    if($("#myNavbar").height() > 30 && $("#myNavbar").height() < 100 ){
       if ($( "#myNavbar li" ).size() > 7){
           $("#myNavbar li:nth-child(6)").after(
-            "<li id='short-menu-button'><a>Více</a></li>"
+            "<li id='short-menu-button'><a>Více <span class='caret'></span></a></li>"
           );
           liNumber = 0;
           $("#myNavbar li").each(function(){
@@ -15,6 +15,13 @@ $(document).ready(function() {
             }
           });
           $("#myNavbar li:nth-child("+ $( "#myNavbar li" ).size() +")").removeClass("short-menu");
+          $("li.short-menu").css("display", "none");
+          $(".navbar-nav > li.short-menu").css("float", "right");
+          $(".navbar-nav > li.short-menu").css("border-top", "1px solid");
+          $(".navbar-nav > li.short-menu").css("clear", "both");
+          $(".navbar-nav > li.short-menu").css("width", "225px");
+          $(".navbar-nav > li.short-menu").css("text-align", "center");
+          $("#short-menu-button a").css("display", "block");
       }
       $( document ).on( "click", "#short-menu-button", function() {
         if ($('.short-menu').is(':visible')) {
@@ -26,4 +33,22 @@ $(document).ready(function() {
         }
       });
     }
+    $(window).resize(function(){
+      if($("#myNavbar").height() > 30 && $("#myNavbar").height() < 100 ){
+          $("li.short-menu").css("display", "none");
+          $(".navbar-nav > li.short-menu").css("float", "right");
+          $(".navbar-nav > li.short-menu").css("border-top", "1px solid");
+          $(".navbar-nav > li.short-menu").css("clear", "both");
+          $(".navbar-nav > li.short-menu").css("width", "225px");
+          $(".navbar-nav > li.short-menu").css("text-align", "center");
+          $("#short-menu-button a").css("display", "block");
+      }else{
+          $("li.short-menu").css("display", "block");
+          $(".navbar-nav > li.short-menu").css("float", "left");
+          $(".navbar-nav > li.short-menu").css("border-top", "none");
+          $(".navbar-nav > li.short-menu").css("clear", "none");
+          $(".navbar-nav > li.short-menu").css("width", "auto");
+          $("#short-menu-button a").css("display", "none");
+      }
+    });
 });
