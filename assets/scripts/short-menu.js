289f16e9f3b68408ddb84ec0f3a9e12faf419a10
Destrosvet <destrosvet@gmail.com>
2017-12-28 20:45:16 +0100

    fix dropdown click outside

--- assets/scripts/short-menu.js
+++ assets/scripts/short-menu.js
@@ -1,38 +1,47 @@
 /**
  * Responsive short menu for version(480px - 768px)
  */
- $(document).ready(function() {
-   $(window).resize(function(){
-     $(".main-menu").css('visibility','hidden');
-     $(".main-menu").show();
-     $(".short-main-menu").hide();
-     if($("#myNavbar").height() > 40 && $("#myNavbar").height() < 100 ){
-       $(".main-menu").hide();
-       $(".short-main-menu").show();
-     }else{
-       $(".main-menu").css('visibility','visible');
-       $(".main-menu").show();
-       $(".short-main-menu").hide();
-     }
-   });
-   $(".main-menu").css('visibility','hidden');
-   $(".main-menu").show();
-   $(".short-main-menu").hide();
-   if($("#myNavbar").height() > 40 && $("#myNavbar").height() < 100 ){
-     $(".main-menu").hide();
-     $(".short-main-menu").show();
-   }else{
-     $(".main-menu").css('visibility','visible');
-     $(".main-menu").show();
-     $(".short-main-menu").hide();
-   }
+$(document).ready(function () {
+    $(window).resize(function () {
+        $(".main-menu").css('visibility', 'hidden');
+        $(".main-menu").show();
+        $(".short-main-menu").hide();
+        if ($("#myNavbar").height() > 40 && $("#myNavbar").height() < 100) {
+            $(".main-menu").hide();
+            $(".short-main-menu").show();
+        } else {
+            $(".main-menu").css('visibility', 'visible');
+            $(".main-menu").show();
+            $(".short-main-menu").hide();
+        }
+    });
+    $(".main-menu").css('visibility', 'hidden');
+    $(".main-menu").show();
+    $(".short-main-menu").hide();
+    if ($("#myNavbar").height() > 40 && $("#myNavbar").height() < 100) {
+        $(".main-menu").hide();
+        $(".short-main-menu").show();
+    } else {
+        $(".main-menu").css('visibility', 'visible');
+        $(".main-menu").show();
+        $(".short-main-menu").hide();
+    }
 
-   $( ".menu-item-has-children > a" ).click(function(event) {
-     event.preventDefault();
-   if($(".sub-menu").is(':visible')){
-     $(".sub-menu").hide();
-   }else{
-     $(".sub-menu").show();
-   }
- });
- });
+    $(".menu-item-has-children > a").click(function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        if ($(".sub-menu").is(':visible')) {
+            $(".sub-menu").hide();
+        } else {
+            $(".sub-menu").show();
+        }
+    });
+    // click outside
+    $(document).click(function () {
+        if ($(".sub-menu").is(':visible')) {
+            console.log($(".sub-menu").is(':visible'));
+            $(".sub-menu").hide();
+        }
+    });
+
+});
