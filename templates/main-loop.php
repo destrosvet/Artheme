2b4148750b47cc35ff941e24cff9913c6f553200
Destrosvet <destrosvet@gmail.com>
2017-12-31 00:08:55 +0100

    fix sidebar and posts

--- templates/main-loop.php
+++ templates/main-loop.php
@@ -6,15 +6,18 @@
 
     <div class="col-md-8 col-sm-12 col-xs-12">
       <div id="posts">
-        <?php $firstSticky = true; ?>
+        <?php //$firstSticky = true;
+            $FeatureArgs = array('posts_per_page' => 1,'post__in'  => get_option( 'sticky_posts' ),'ignore_sticky_posts' => 1 );
+            $FeaturePost = new WP_Query($FeatureArgs);
+        ?>
 
          <?php $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
             if ( is_home() ) {
                 $query = new WP_Query(array(
                 'paged' => $paged,
-                'posts_per_page' => 10,
+                'posts_per_page' => 8,
                 'category_name' => 'arena',
-
+                'post__not_in' => array($FeaturePost->post->ID),
                 ));
             } else
             {
@@ -29,11 +32,11 @@
          ?>
     		<?php while ($query -> have_posts()) : $query->the_post(); ?>
             <?php
-                if (is_sticky() && $firstSticky)
+/*                if (is_sticky() && $firstSticky)
                 {
                     $firstSticky == false;
                 //    continue;
-                }
+                }*/
                 get_template_part('templates/post', artalk_get_current_category() );
             ?>
 
