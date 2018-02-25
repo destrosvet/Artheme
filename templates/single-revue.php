01c4c79ec7266db90c713083352745c9a5cfa8d0
mato slebo <matejslebo@gmail.com>
2018-02-25 15:25:00 +0100

    fix revue responsive

--- templates/single-revue.php
+++ templates/single-revue.php
@@ -46,35 +46,40 @@
             <div class="col-lg-9 col-sm-12 col-xs-12 padding-sm-12 single-revue single-text-container">
 
                 <?php the_content(); ?>
+                <div class="clear"></div>
+            </div>
 
                 <?php endwhile; elseif ( have_posts()): while (! isset ( $loop_first ) && have_posts()) : the_post(); ?>
 
                 <?php $loop_first = 1; ?>
-                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-                  <div class=" revue-single-title">
-                      <h1><?php the_title(); ?></h1>
-                  </div>
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 revue-single-title">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+                <div class="col-lg-8 col-sm-12 col-xs-12 padding-sm-12">
                   <div class=" post-meta-single">
                       <span><?php the_author_posts_link(); ?></span> | <time><?php the_time( get_option( 'date_format' ) ); ?></time>
                       <span class="post-meta-single-category"> |
                           <?php do_action('artalk_post_cats','',array('separator' => ' ','main-category' => true),true,true); ?>
-                  </span>
+                      </span>
                   </div>
-                </div>
-                  <div class="col-lg-4  col-md-12 col-sm-12 col-xs-12 revue-download-block">
+                  <div class="col-lg-4  col-md-12 col-sm-12 col-xs-12 revue-download-block-mobile">
                       <?php get_revue_download() ?>
                   </div>
-                <div class="col-lg-9 col-sm-12 col-xs-12 padding-sm-12 single-revue single-text-container">
+                  <div class=" single-revue single-text-container">
 
-                    <?php the_content(); ?>
-
+                      <?php the_content(); ?>
+                      <div class="clear"></div>
+                  </div>
+                </div>
+                <div class="col-lg-4  col-md-12 col-sm-12 col-xs-12 revue-download-block">
+                    <?php get_revue_download() ?>
+                </div>
                     <?php endwhile; else:?>
                     <h3>Omlouváme se, nic jsme nenašli.</h3>
                 <?php endif;?>
 
 
-                <div class="clear"></div>
-            </div>
+
 
         </article>
         <div class="col-lg-12 col-md-12 col-xs-12 norightpadding noleftpadding no-padding-right-md">
