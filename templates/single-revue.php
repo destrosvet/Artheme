c7b09ee136016382d34272fcc146a6719e5ebc15
mato slebo <matejslebo@gmail.com>
2018-02-25 14:28:43 +0100

    fix revue responsive

--- templates/single-revue.php
+++ templates/single-revue.php
@@ -50,17 +50,18 @@
                 <?php endwhile; elseif ( have_posts()): while (! isset ( $loop_first ) && have_posts()) : the_post(); ?>
 
                 <?php $loop_first = 1; ?>
-                <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 revue-single-title">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 revue-single-title">
                     <h1><?php the_title(); ?></h1>
                 </div>
-                <div class="post-meta-single ">
+                <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 post-meta-single">
                     <span><?php the_author_posts_link(); ?></span> | <time><?php the_time( get_option( 'date_format' ) ); ?></time>
                     <span class="post-meta-single-category"> |
                         <?php do_action('artalk_post_cats','',array('separator' => ' ','main-category' => true),true,true); ?>
                 </span>
                 </div>
-                <?php get_revue_download() ?>
-
+                <div class="col-lg-4  col-md-12 col-sm-12 col-xs-12 revue-download-block">
+                    <?php get_revue_download() ?>
+                </div>
                 <div class="col-lg-9 col-sm-12 col-xs-12 padding-sm-12 single-revue single-text-container">
 
                     <?php the_content(); ?>
