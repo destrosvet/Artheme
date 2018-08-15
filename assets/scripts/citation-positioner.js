3e632c3d724f7a6f6c4059d733f92d0fda1bc882
Destrosvet <destrosvet@gmail.com>
2018-08-15 16:03:22 +0200

    - fix collision with figures

--- assets/scripts/citation-positioner.js
+++ assets/scripts/citation-positioner.js
@@ -5,6 +5,28 @@
 
 $(document).ready(function() {
     if ($( window ).width() > 1200) {
+        $.each($('.citate-left'), function (index, value) {
+            colliders_selector = this;
+
+            $.each($('figure'), function (index, value) {
+                figure_selector = this;
+                if (colliders_selector != figure_selector && $(colliders_selector).collision(figure_selector).length !== 0) {
+
+
+                    figure_center = $(figure_selector).offset().top + ($(figure_selector).outerHeight(true) / 2);
+                    coliders_selector_bottom = $(colliders_selector).offset().top + $(colliders_selector).outerHeight(true);
+
+                    if (coliders_selector_bottom < figure_center) {
+                        offsetPosition = $(colliders_selector).offset().top + $(colliders_selector).outerHeight(true) - $(figure_selector).offset().top + 20;
+                        console.log(offsetPosition);
+                        console.log($(colliders_selector).collision(figure_selector));
+                        $(figure_selector).collision(colliders_selector).css({"top": "-=" + offsetPosition + "px"});
+                    }
+
+                }
+            });
+
+        });
 
         $.each($('.citate-left'), function (index, value) {
             colliders_selector = this;
@@ -12,8 +34,18 @@ $(document).ready(function() {
             $.each($('.citate-left'), function (index, value) {
                 obstacles_selector = this;
                 if (colliders_selector != obstacles_selector && $(colliders_selector).collision(obstacles_selector).length !== 0) {
-                    offsetPosition = $(colliders_selector).offset().top + $(colliders_selector).outerHeight(true) - $(obstacles_selector).offset().top + 20;
-                    $(colliders_selector).collision(obstacles_selector).css({"top": "+=" + offsetPosition + "px"});
+                        offsetPosition = $(colliders_selector).offset().top + $(colliders_selector).outerHeight(true) - $(obstacles_selector).offset().top + 20;
+                        objectInCollision = $(colliders_selector).collision(obstacles_selector);
+                        objectInCollision.css({"top": "+=" + offsetPosition + "px"});
+
+
+                        $.each($('figure'), function (index, value) {
+                            figure_selector = this;
+                            if ($(figure_selector).collision(objectInCollision).length !== 0 ) {
+                                offsetPosition = $(figure_selector).outerHeight(true);
+                                $(figure_selector).collision(objectInCollision).css({"top": "+=" + offsetPosition + "px"});
+                            }
+                    });
 
                 }
             });
